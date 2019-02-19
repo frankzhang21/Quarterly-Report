5f9e96522b67ac9d270da62666c2a976d22be5a5
frankzhang21 <40694030+frankzhang21@users.noreply.github.com>
2019-02-19 15:50:22 +1100

    Format client name based on the ID. All names follow the latest name.

---
+++ Format media database.R
@@ -0,0 +1,22 @@
+library(readxl)
+library(data.table)
+library(writexl)
+
+db <- read_excel(file.choose(),sheet = "Database(Media)",col_types = "text")
+
+setDT(db)
+
+
+
+setorder(db,-`Year and Quarter`)
+db[,print(.SD[1,16]),by="Client ID"]
+
+a <- split(db,by="Client ID")
+
+for (i in 1:1860) {
+  a[[i]][,"Client Name":=.(a[[i]]$`Client Name`[1])]
+}
+
+final <- rbindlist(a)
+
+write_xlsx(final,"H:/Report/Quarterly/Media/eli/a.xlsx")
